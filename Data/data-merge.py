c6cea047e5b1cc3c423bb4e2dd54b88d82597f31
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-10-10 00:29:06 -0500

    :sparkles: final data creation

--- Data/data-merge.py
+++ Data/data-merge.py
@@ -9,32 +9,21 @@ data_save = os.path.join(pathlib.Path(__file__).parent.absolute(), 'final')
 """
 DATA IMPORT
 """
-africa_countries = pd.read_csv(os.path.join(processed_dir, 'africa_countries.csv'))
-countries_gdp = pd.read_csv(os.path.join(data_save, 'countries_gdp.csv'))
-economic_outlook = pd.read_csv(os.path.join(data_save, 'economic_outlook.csv'))
-hdr_data = pd.read_csv(os.path.join(data_save, 'hdr_data.csv'))
+countries_population = pd.read_csv(os.path.join(processed_dir, 'countries_population.csv'))
+economic_outlook = pd.read_csv(os.path.join(processed_dir, 'economic_outlook.csv'))
+gini_index = pd.read_csv(os.path.join(processed_dir, 'gini_index.csv'))
+hdr_data = pd.read_csv(os.path.join(processed_dir, 'hdr_data.csv'))
 
 
 """
 DATA MERGING
 """
-# Merge africa_countries with economic_outlook on Country Code withouth africa countries Country Name column
-africa_countries.drop(columns=['Country Name'], inplace=True)
-africa_countries = africa_countries.merge(economic_outlook, on='Country Code', how='left')
+africa_countries = economic_outlook
 
-# Drop duplicates
-africa_countries.drop_duplicates(subset=['Country Code'], inplace=True)
+# Merge the data
+africa_countries = africa_countries.merge(countries_population, on='Country Code', how='left').merge(gini_index, on='Country Code', how='left').merge(hdr_data, on='Country Code', how='left')
 
-# Drop nan Country name rows
-africa_countries.dropna(subset=['Country Name'], inplace=True)
+print(africa_countries.info())
 
-# Merge africa_countries with countries_gdp on Country Name
-africa_countries = africa_countries.merge(countries_gdp, on='Country Name', how='left')
-
-# Order columns (first Country Name and Country Code) and sort the others alphabetically
-columns = ['Country Name', 'Country Code'] + sorted([col for col in africa_countries.columns if col not in ['Country Name', 'Country Code']])
-africa_countries = africa_countries[columns]
-
-# Save the result to a CSV file
+# Save the result to CSV
 africa_countries.to_csv(os.path.join(data_save, 'africa_countries.csv'), index=False)
-
--- Data/data_extraction.py
+++ Data/data_extraction.py
@@ -5,94 +5,50 @@ import pathlib
 # Extract data From Banco Mundial
 data_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), 'raw')
 processed_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), 'processed')
+
 """
+# World bank population data
 """
- # Banco mundial data
-"""
-# Data from https://datos.bancomundial.org/indicador/SP.POP.TOTL?locations=A9
-countries_metadata = pd.read_excel(
-    os.path.join(data_dir, 'poblacion-banco-mundial.xlsx'),
-    sheet_name='Metadata - Countries'
-)
+# Data from https://datos.bancomundial.org/indicador/SP.POP.TOTL
 countries_population = pd.read_excel(
-    os.path.join(data_dir, 'poblacion-banco-mundial.xlsx'),
+    os.path.join(data_dir, 'world-bank-population.xlsx'),
     sheet_name='Data',
     skiprows=3
 )
-
-# Create a column with the average population
-countries_population['Average Population'] = countries_population[['2016', '2017','2018', '2019', '2020']].mean(axis=1)
-
-# Drop rows with country name and region as NaN
-countries_metadata.dropna(subset=['Country Name'], inplace=True)
-countries_metadata.dropna(subset=['Region'], inplace=True)
-
-# Select countries with region containing África
-africa_countries = countries_metadata[countries_metadata['Region'].str.contains('África')]
-
 # Rename column income group
-africa_countries = africa_countries.rename(columns={'Income_Group': 'Income group'})
-countries_population = countries_population.rename(columns={'2020' : 'Population 2020'})
+countries_population = countries_population.rename(columns={'2019' : 'Population 2019'})
 
 # Select relevant columns
-africa_countries = africa_countries[['Country Name', 'Country Code', 'Income group', 'Region']]
-countries_population = countries_population[['Country Code', 'Population 2020', 'Average Population']]
+countries_population = countries_population[['Country Code', 'Population 2019']]
 
-# Merge with population data
-africa_countries = africa_countries.merge(countries_population, on='Country Code', how='left')
-africa_countries = africa_countries.sort_values('Country Name')
-
-# Save the result to a CSV file
-africa_countries.to_csv(os.path.join(processed_dir, 'africa_countries.csv'), index=False)
+# Save the result to CSV
+countries_population.to_csv(os.path.join(processed_dir, 'countries_population.csv'), index=False)
 
 """
-# Data Africa
+# World bank Gini index data
 """
-# Data from https://www.kaggle.com/datasets/ivanbyone/population-and-gdp-africa
-countries_gdp = pd.read_csv(os.path.join(data_dir, 'Data_Africa.csv'))
-
-# Strip extra spaces from column names
-countries_gdp.columns = countries_gdp.columns.str.strip()
-
-# Pivot the DataFrame to have 'Year' as columns and 'Population' and 'GDP (USD)' as values
-countries_gdp = countries_gdp.pivot(
-    index=['Country', 'Continent'], 
-    columns='Year',
-    values=['Population', 'GDP (USD)']
+# Data from https://data.worldbank.org/indicator/SI.POV.GINI
+gini_index = pd.read_excel(
+    os.path.join(data_dir, 'world-bank-gini.xlsx'),
+    sheet_name='Data',
+    skiprows=3
 )
+# Rename column income group
+gini_index = gini_index.rename(columns={'2019' : 'Gini Index 2019'})
 
-# Flatten the column names for easier access
-countries_gdp.columns = [f'{col[0]}_{col[1]}' for col in countries_gdp.columns]
-countries_gdp = countries_gdp.reset_index()
+# Select relevant columns
+gini_index = gini_index[['Country Code', 'Gini Index 2019']]
 
-# Create columns with the average population and GDP over the last 5 years
-years = ['2016', '2017', '2018', '2019', '2020']
-population_columns = [f'Population_{year}' for year in years]
-gdp_columns = [f'GDP (USD)_{year}' for year in years]
-
-countries_gdp['Average Population'] = countries_gdp[population_columns].mean(axis=1)
-countries_gdp['Average GDP (USD)'] = countries_gdp[gdp_columns].mean(axis=1)
-
-# Rename column Country to Country Name
-countries_gdp = countries_gdp.rename(columns={'Country': 'Country Name'})
-
-# Select the relevant columns
-countries_gdp = countries_gdp[['Country Name', 'Continent', 'Average Population', 'Average GDP (USD)']]
-
-# Save the result to a CSV file
-countries_gdp.to_csv(os.path.join(processed_dir, 'countries_gdp.csv'), index=False)
+# Save the result to CSV
+gini_index.to_csv(os.path.join(processed_dir, 'gini_index.csv'), index=False)
 
 """
-# Economic outlook
+# African Economic Outlook Data
 """
-# https://www.kaggle.com/datasets/waalbannyantudre/african-economic-outlook
-# Download URL:,http://dataportal.opendataforafrica.org/mhuiccf/african-economic-outlook-january-2019
-# Source: African Development Bank
-
 economic_outlook = pd.read_csv(os.path.join(data_dir, 'african-economic-outlook.csv'))
 
 # Define years and extract unique indicators
-years = ['2016', '2017', '2018', '2019', '2020']
+years = ['2019']
 economic_indicators = economic_outlook['Indicators Name'].unique()
 
 # Pivot the DataFrame
@@ -103,45 +59,63 @@ economic_outlook = economic_outlook.pivot(
 )
 
 # Flatten column names
-economic_outlook.columns = ['_'.join(map(str, col)) for col in economic_outlook.columns]
+economic_outlook.columns = [' '.join(map(str, col[::-1])) for col in economic_outlook.columns]
 economic_outlook = economic_outlook.reset_index()
 
-# Create average columns for each indicator
-for indicator in economic_indicators:
-    indicator_columns = [f"{year}_{indicator}" for year in years]
-    economic_outlook[f'Average {indicator}'] = economic_outlook[indicator_columns].mean(axis=1)
-
 # Rename columns
-economic_outlook = economic_outlook.rename(columns={'Country and Regions': 'Country Code'})
-economic_outlook = economic_outlook.rename(columns={'Country and Regions Name': 'Country Name'})
+economic_outlook = economic_outlook.rename(columns={'Country and Regions': 'Country Code', 'Country and Regions Name': 'Country Name'})
 
-# Select relevant columns
-selected_columns = ['Country and Regions', 'Country and Regions Name', 'Country and Regions - RegionId'] + \
-                   [f'Average {indicator}' for indicator in economic_indicators]
-important_columns = [
-    'Country Code', 'Country Name', 
-    'Average Real GDP growth (annual %)', 'Average Real per Capita GDP Growth Rate (annual %)',
-    'Average Gross domestic product, (constant prices US$)', 'Average Gross domestic product, current prices (current US$)',
-    'Average Final consumption expenditure  (current US$)', 'Average Household final consumption expenditure (current US$)',
-    'Average Gross capital formation (current US$)', 'Average Exports of goods and services (current US$)',
-    'Average Imports of goods and services (current US$)', 'Average General government final consumption expenditure (current US$)',
-    'Average Inflation, consumer prices (annual %)', 'Average Current account balance (Net, BoP, cur. US$)', 
-    'Average Central government, Fiscal Balance (% of GDP)'
+# Filtrar las columnas necesarias
+columns_to_use = [
+    'Country Code', 'Country Name', 'Central government, Fiscal Balance (% of GDP) 2019', 
+    'Current account balance (As % of GDP) 2019', 'Exports of goods and services (% of GDP) 2019',
+    'Imports of goods and services (% of GDP) 2019', 'Inflation, consumer prices (annual %) 2019',
+    'Gross capital formation (% of GDP) 2019', 'Final consumption expenditure  (% of GDP) 2019',
+    'General government final consumption expenditure (% of GDP) 2019',
+    'Household final consumption expenditure  (% of GDP) 2019',
+    'Real GDP growth (annual %) 2019'
 ]
-# Filter the DataFrame to select only the relevant columns
-economic_outlook = economic_outlook[important_columns]
 
-# Save the result to CSV
+# Filtrar las columnas seleccionadas
+economic_outlook = economic_outlook[columns_to_use]
+
+# Guardar el resultado en un archivo CSV
 economic_outlook.to_csv(os.path.join(processed_dir, 'economic_outlook.csv'), index=False)
-"""
 
 """
-# HDR DATA
+# Human Development Report Data
 """
-# Data from https://hdr.undp.org/data-center/documentation-and-downloads (HDR Data)
 hdr_data = pd.read_excel(os.path.join(data_dir, 'hdr-data.xlsx'))
 
+# Eliminar las columnas innecesarias
+hdr_data.drop(columns=['dimension', 'note'], inplace=True)
 
+# Renombrar columnas
+hdr_data = hdr_data.rename(columns={'countryIsoCode': 'Country Code'})
 
+# Pivot the DataFrame
+hdr_data = hdr_data.pivot(
+    index=['Country Code'], 
+    columns='indicator', 
+    values='value'
+)
+
+# Flatten column names
+hdr_data = hdr_data.reset_index()
+
+# Filtrar las columnas necesarias del HDR
+hdr_columns_to_use = [
+    'Country Code', 'Gross National Income Per Capita (2017 PPP$)', 'Human Development Index (value)', 
+    'Inequality-adjusted Human Development Index (value)', 'Life Expectancy at Birth (years)', 
+    'Labour force participation rate, female (% ages 15 and older)', 
+    'Labour force participation rate, male (% ages 15 and older)',
+    'Maternal Mortality Ratio (deaths per 100,000 live births)', 
+    'Share of seats in parliament, female (% held by women)', 
+    'Expected Years of Schooling (years)'
+]
+
+# Filtrar las columnas seleccionadas
+hdr_data = hdr_data[hdr_columns_to_use]
+
+# Guardar el resultado en un archivo CSV
 hdr_data.to_csv(os.path.join(processed_dir, 'hdr_data.csv'), index=False)
-
